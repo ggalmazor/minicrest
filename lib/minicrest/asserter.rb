819b0dcdee0fc61471344b21d5df9ef7e17c38a2
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2026-01-17 19:23:08 +0100

    chore: Add new assertion methods and matchers for enhanced flexibility
    - Added multiple assertion methods: `descends_from`, `instance_of`, `nil_value`, `truthy`, `falsy`, `responds_to`, `starts_with`, `ends_with`, `matches_pattern`, `blank`, `empty`, `has_size`, `is_greater_than`, `is_less_than`, `is_greater_than_or_equal_to`, `is_less_than_or_equal_to`, `between`, `is_close_to`, `includes`, `has_key`, `has_value`, `contains`, `contains_exactly`, `all_items`, `some_items`, `no_items`, `all_entries`, `some_entry`, `no_entry`, `is_in`, `has_attribute`.
    - Added method aliases: `is_not` and `does_not` for `never`.
    - Enhanced test assertion capabilities with improved API coverage.

--- lib/minicrest/asserter.rb
+++ lib/minicrest/asserter.rb
@@ -34,11 +34,6 @@ module Minicrest
     #
     # @param expected [Object] the expected value
     # @return [true] if assertion passes
-    # @raise [Minitest::Assertion] if assertion fails
-    #
-    # @example
-    #   assert_that(42).equals(42)
-    #   assert_that([1, 2]).equals([1, 2])
     def equals(expected)
       matches(Equals.new(expected))
     end
@@ -47,13 +42,255 @@ module Minicrest
     #
     # @param expected [Object] the expected object reference
     # @return [true] if assertion passes
-    # @raise [Minitest::Assertion] if assertion fails
-    #
-    # @example
-    #   obj = Object.new
-    #   assert_that(obj).is(obj)
     def is(expected)
       matches(Is.new(expected))
+    end
+
+    # Asserts that actual is an instance of expected_type.
+    #
+    # @param expected_type [Class, Module] the expected type
+    # @return [true] if assertion passes
+    def descends_from(expected_type)
+      matches(DescendsFrom.new(expected_type))
+    end
+
+    # Asserts that actual is an instance of expected_type.
+    #
+    # @param expected_type [Class] the expected type
+    # @return [true] if assertion passes
+    def instance_of(expected_type)
+      matches(InstanceOf.new(expected_type))
+    end
+
+    # Asserts that actual is nil.
+    #
+    # @return [true] if assertion passes
+    def nil_value
+      matches(NilValue.new)
+    end
+
+    # Asserts that actual is truthy.
+    #
+    # @return [true] if assertion passes
+    def truthy
+      matches(Truthy.new)
+    end
+
+    # Asserts that actual is falsy.
+    #
+    # @return [true] if assertion passes
+    def falsy
+      matches(Falsy.new)
+    end
+
+    # Asserts that actual responds to given methods.
+    #
+    # @param methods [Array<Symbol>] the methods to check
+    # @return [true] if assertion passes
+    def responds_to(*methods)
+      matches(RespondsTo.new(*methods))
+    end
+
+    # Asserts that actual starts with prefix.
+    #
+    # @param prefix [String] the prefix
+    # @return [true] if assertion passes
+    def starts_with(prefix)
+      matches(StartsWith.new(prefix))
+    end
+
+    # Asserts that actual ends with suffix.
+    #
+    # @param suffix [String] the suffix
+    # @return [true] if assertion passes
+    def ends_with(suffix)
+      matches(EndsWith.new(suffix))
+    end
+
+    # Asserts that actual matches pattern.
+    #
+    # @param pattern [Regexp] the pattern
+    # @return [true] if assertion passes
+    def matches_pattern(pattern)
+      matches(MatchesPattern.new(pattern))
+    end
+
+    # Asserts that actual is blank.
+    #
+    # @return [true] if assertion passes
+    def blank
+      matches(Blank.new)
+    end
+
+    # Asserts that actual is empty.
+    #
+    # @return [true] if assertion passes
+    def empty
+      matches(Empty.new)
+    end
+
+    # Asserts that actual has expected size.
+    #
+    # @param expected [Integer, Matcher] the size or matcher
+    # @return [true] if assertion passes
+    def has_size(expected)
+      matches(HasSize.new(expected))
+    end
+
+    # Asserts that actual is greater than expected.
+    #
+    # @param expected [Comparable] the value
+    # @return [true] if assertion passes
+    def is_greater_than(expected)
+      matches(IsGreaterThan.new(expected))
+    end
+
+    # Asserts that actual is less than expected.
+    #
+    # @param expected [Comparable] the value
+    # @return [true] if assertion passes
+    def is_less_than(expected)
+      matches(IsLessThan.new(expected))
+    end
+
+    # Asserts that actual is greater than or equal to expected.
+    #
+    # @param expected [Comparable] the value
+    # @return [true] if assertion passes
+    def is_greater_than_or_equal_to(expected)
+      matches(IsGreaterThanOrEqualTo.new(expected))
+    end
+
+    # Asserts that actual is less than or equal to expected.
+    #
+    # @param expected [Comparable] the value
+    # @return [true] if assertion passes
+    def is_less_than_or_equal_to(expected)
+      matches(IsLessThanOrEqualTo.new(expected))
+    end
+
+    # Asserts that actual is between min and max.
+    #
+    # @param min [Comparable] minimum
+    # @param max [Comparable] maximum
+    # @param exclusive [Boolean] whether exclusive
+    # @return [true] if assertion passes
+    def between(min, max, exclusive: false)
+      matches(Between.new(min, max, exclusive:))
+    end
+
+    # Asserts that actual is close to expected.
+    #
+    # @param expected [Numeric] expected
+    # @param delta [Numeric] delta
+    # @return [true] if assertion passes
+    def is_close_to(expected, delta)
+      matches(IsCloseTo.new(expected, delta))
+    end
+
+    # Asserts that actual includes items.
+    #
+    # @param items [Array] items
+    # @return [true] if assertion passes
+    def includes(*items)
+      matches(Includes.new(*items))
+    end
+
+    # Asserts that actual has keys.
+    #
+    # @param keys [Array] keys
+    # @return [true] if assertion passes
+    def has_key(*keys)
+      matches(HasKey.new(*keys))
+    end
+
+    # Asserts that actual has values.
+    #
+    # @param values [Array] values
+    # @return [true] if assertion passes
+    def has_value(*values)
+      matches(HasValue.new(*values))
+    end
+
+    # Asserts that actual contains items in any order.
+    #
+    # @param items [Array] items
+    # @return [true] if assertion passes
+    def contains(*items)
+      matches(Contains.new(*items))
+    end
+
+    # Asserts that actual contains items in order.
+    #
+    # @param items [Array] items
+    # @return [true] if assertion passes
+    def contains_exactly(*items)
+      matches(ContainsExactly.new(*items))
+    end
+
+    # Asserts that all items match.
+    #
+    # @param item_matcher [Matcher] matcher
+    # @return [true] if assertion passes
+    def all_items(item_matcher)
+      matches(AllItems.new(item_matcher))
+    end
+
+    # Asserts that some items match.
+    #
+    # @param item_matcher [Matcher] matcher
+    # @return [true] if assertion passes
+    def some_items(item_matcher)
+      matches(SomeItems.new(item_matcher))
+    end
+
+    # Asserts that no items match.
+    #
+    # @param item_matcher [Matcher] matcher
+    # @return [true] if assertion passes
+    def no_items(item_matcher)
+      matches(NoItems.new(item_matcher))
+    end
+
+    # Asserts that all entries match.
+    #
+    # @param entry_matcher [Proc, Matcher] matcher
+    # @return [true] if assertion passes
+    def all_entries(entry_matcher)
+      matches(AllEntries.new(entry_matcher))
+    end
+
+    # Asserts that at least one entry matches.
+    #
+    # @param entry_matcher [Proc, Matcher] matcher
+    # @return [true] if assertion passes
+    def some_entry(entry_matcher)
+      matches(SomeEntry.new(entry_matcher))
+    end
+
+    # Asserts that no entries match.
+    #
+    # @param entry_matcher [Proc, Matcher] matcher
+    # @return [true] if assertion passes
+    def no_entry(entry_matcher)
+      matches(NoEntry.new(entry_matcher))
+    end
+
+    # Asserts that actual is in collection.
+    #
+    # @param collection [Enumerable] collection
+    # @return [true] if assertion passes
+    def is_in(collection)
+      matches(IsIn.new(collection))
+    end
+
+    # Asserts that actual has attribute.
+    #
+    # @param name [Symbol] attribute name
+    # @param value_matcher [Matcher, nil] value matcher
+    # @return [true] if assertion passes
+    def has_attribute(name, value_matcher = nil)
+      matches(HasAttribute.new(name, value_matcher))
     end
 
     # Asserts that actual does NOT match the given matcher.
@@ -68,6 +305,8 @@ module Minicrest
     def never(matcher)
       matches(Not.new(matcher))
     end
+    alias is_not never
+    alias does_not never
 
     # Asserts that actual matches the given matcher.
     #
